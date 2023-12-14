f2757f5de4a928b3195fcccb301a69ee8c12bf9a
surya <eksurya27@gmail.com>
2023-12-14 23:47:47 +0530

    Added pytest report generation functionality

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -1,3 +1,6 @@
+import os
+from datetime import datetime
+
 import pytest
 from selenium import webdriver
 from selenium.webdriver.edge import service
@@ -28,3 +31,25 @@ def pytest_addoption(parser):
 @pytest.fixture()
 def browser(request):
     return request.config.getoption("--browser")
+
+
+########### pytest HTML Report ################
+
+
+# It is hook for Adding Environment info to HTML Report
+def pytest_configure(config):
+    config._metadata['Project Name'] = 'automationpractice.pl'
+    config._metadata['Module Name'] = 'Women shop'
+    config._metadata['Tester'] = 'Surya'
+
+# It is hook for delete/Modify Environment info to HTML Report
+@pytest.mark.optionalhook
+def pytest_metadata(metadata):
+    metadata.pop("JAVA_HOME", None)
+    metadata.pop("Plugins", None)
+
+
+#Specifying report folder location and save report with timestamp
+@pytest.hookimpl(tryfirst=True)
+def pytest_configure(config):
+    config.option.htmlpath = os.path.abspath(os.curdir)+"\\reports\\"+datetime.now().strftime("%d-%m-%Y %H-%M-%S")+".html"
