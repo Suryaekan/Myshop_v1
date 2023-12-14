7c49735f90c780f4e6a0bbc01295c5b0d6d9bcb5
surya <eksurya27@gmail.com>
2023-12-14 23:26:03 +0530

    Added mutiple browser testing functionality

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -1,9 +1,30 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.edge import service
+
 
 @pytest.fixture
-def setup():
-    options = webdriver.ChromeOptions()
-    options.add_experimental_option("detach", True)
-    driver = webdriver.Chrome(options=options)
+def setup(browser):
+    if browser == "edge":
+        options = webdriver.EdgeOptions()
+        options.add_experimental_option("detach", True)
+        driver = webdriver.Edge(options=options)
+        print("Launching Edge")
+    elif browser == "firefox":
+        options = webdriver.FirefoxOptions()
+        driver = webdriver.Firefox(options=options)
+
+    else:
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option("detach", True)
+        driver = webdriver.Chrome(options=options)
     return driver
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser")
+
+
+@pytest.fixture()
+def browser(request):
+    return request.config.getoption("--browser")
--- utilities/customLogger.py
+++ utilities/customLogger.py
@@ -5,8 +5,7 @@ import os
 class LogGen:
     @staticmethod
     def loggen():
-        path = os.path.abspath(os.curdir)+'//logs//automation/log'
-        logging.basicConfig(filename=path, format='%(asctime)s: %(levelname)s: %(message)s:',
+        logging.basicConfig(filename="..\\logs\\automation.log", format='%(asctime)s: %(levelname)s: %(message)s',
                             datefmt='%m/%d/%Y %I:%M:%S %p')
         logger = logging.getLogger()
         logger.setLevel(logging.DEBUG)
