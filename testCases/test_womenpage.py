5fd8996d2ae8678234cbc4e2cdcc23dfe7a253a9
surya <eksurya27@gmail.com>
2023-12-14 21:30:25 +0530

    Adding logger file to project

--- testCases/test_womenpage.py
+++ testCases/test_womenpage.py
@@ -4,15 +4,16 @@ from PageObjects.Page_Women import PageWomen
 from PageObjects.Product_page import Product
 from testCases.conftest import setup
 from utilities.readProperties import ReadConfig
+from utilities.customLogger import LogGen
 
-
-class Test_womenpage:
+class TestWomenPage:
 
     def test_wpage(self, setup):
+        logger = LogGen.loggen()
+        logger.info("*** Test_womenpage started ***")
         self.driver = setup
-        url = ReadConfig.getapplicationurl()
-        time.sleep(2)
-        self.driver.get(url)
+        self.driver.get("http://www.automationpractice.pl/index.php?id_category=3&controller=category")
+        logger.info("Launching Application")
         self.driver.maximize_window()
         self.wp = PageWomen(self.driver)
         time.sleep(2)
@@ -23,26 +24,45 @@ class Test_womenpage:
 
         # self.wp.click_quickview()
 
+        logger.info("Click on a product")
         self.wp.click_product()
         self.pp = Product(self.driver)
+        logger.info("Switching to frame 1")
         self.pp.switch_productframe()
+        logger.info("Setting quantity to 4")
         self.pp.set_quantity(4)
+        logger.info("Clicking quantity increase button")
         self.pp.increase_quantity()
+        logger.info("Clicking quantity decrease button")
         self.pp.decrease_quantity()
+        logger.info("choosing dropdown Small")
         self.pp.click_s_m_l("S")
+        logger.info("choosing dropdown Large")
         self.pp.click_s_m_l("L")
+        logger.info("choosing dropdown Medium")
         self.pp.click_s_m_l("M")
+        logger.info("choosing color White")
         self.pp.click_white()
+        logger.info("choosing color Black")
         self.pp.click_black()
         # time.sleep(1)
+        logger.info("Clicking add to cart button")
         self.pp.add_to_cart()
         time.sleep(4)
         # self.pp.continue_shopping()
+        logger.info("Clicking second product")
         self.wp.click_product2()
         time.sleep(1)
+        logger.info("Switching to frame 1")
         self.pp.switch_productframe()
+        logger.info("choosing dropdown Large")
         self.pp.click_s_m_l("L")
+        logger.info("choosing color Blue")
         self.pp.color_blue()
+        logger.info("choosing color Orange")
         self.pp.color_orange()
+        logger.info("Clicking add to cart button")
         self.pp.add_to_cart()
+        logger.info("Clicking checkout button")
         self.pp.checkout()
+        logger.info("*** Test_womenpage Finished ***")
